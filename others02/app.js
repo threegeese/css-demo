6c49b2890b87c2f15546bfe22178b11c394816a0
threegeese <19970913qjh@gmail.com>
2019-08-16 16:13:46 +0800

    :rocket:

--- others02/app.js
+++ others02/app.js
@@ -1,27 +1,44 @@
 import 'https://cdn.bootcss.com/vue/2.6.10/vue.min.js'
 
-// // 持久本地存储
-// const STORAGE_KEY = 'TodoMVC'
+// 持久本地存储
+const STORAGE_KEY = 'TodoMVC'
 
-// const todoStorage = {
-//   fetch () {
-//     let todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
-//     todos.forEach((todo, index) => {
-//       todo.id = index
-//     })
-//   },
-//   save (todo) {
-
-//   }
-// }
+const todoStorage = {
+  fetch () {
+    let todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+    todos.forEach((todo, index) => {
+      todo.id = index
+    })
+    return todos
+  },
+  save (todo) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todo))
+  }
+}
 
 new Vue({
   data: {
+    todos: todoStorage.fetch(),
     newTodo: ''
+  },
+  watch: {
+    todos: {
+      handler (todo) {
+        todoStorage.save(todo)
+      },
+      deep: true
+    }
   },
   methods: {
     addTodo () {
-      
+      let value = this.newTodo && this.newTodo.trim()
+      if (value) {
+        this.todos.push({
+          title: value
+        })
+        this.newTodo = ''
+      }
+      return
     }
   }
 }).$mount('#app')
